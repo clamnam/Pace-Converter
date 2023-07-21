74abec6be3233d4c166ddabcf8d128ba2ea13577
clamnam <n00211151@iadt.ie>
2023-07-21 14:18:39 +0100

    m-km working

--- gui.py
+++ gui.py
@@ -1,23 +1,51 @@
 import tkinter as tk
 from tkinter import ttk
+val=0
+window = tk.Tk()
 
+num_var=tk.StringVar()
+
+
+
+# defining a function that will
+# get the name and password and
+# print them on the screen
+def submit():
+
+	num=num_var.get()
+	num =calculator(num)
+	print("The num is : " + str(num))
+	
+	num_var.set("")
+	
+	
 
 
 def calculator(num):
-    return num*1.60934
+    # (num) 
+    return float(num)*1.60934
 
-window = tk.Tk()
+
+
 window.title("Conversion Calculator")
+
+
+# creating a entry for input
+# num using widget Entry
+num_entry = tk.Entry(window,textvariable = num_var, font=('calibre',10,'normal'))
+
 label = tk.Label( text="Welcome to the conversion calculator", pady=30, padx=100, font=100,)
-click_here = tk.Button(window, text="Click to Convert", command=calculator, padx = 10, pady = 10)
-w = tk.Entry(window)
+# entry=tk.Entry(window, textvariable= val, font = ('calibre',10,'normal'))
+
+click_here = tk.Button(window, text="Click to Convert", command=submit, padx = 10, pady = 10)
+
 # root.bind("<Return>", calculate)
 
 
 
 
 label.pack()
-w.pack()
+num_entry.pack()
 click_here.pack()
 
 window.mainloop() 
