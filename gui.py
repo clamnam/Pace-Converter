b3c3d04479ed53378fa20f3096e6fe365c67b7a6
clamnam <n00211151@iadt.ie>
2023-08-07 16:41:18 +0100

    radio select added

--- gui.py
+++ gui.py
@@ -4,8 +4,9 @@ val=0
 window = tk.Tk()
 
 num_var=tk.StringVar()
+str_var=tk.StringVar()
 
-
+# 
 
 # defining a function that will
 # get the name and password and
@@ -33,6 +34,8 @@ window.title("Conversion Calculator")
 # creating a entry for input
 # num using widget Entry
 num_entry = tk.Entry(window,textvariable = num_var, font=('calibre',10,'normal'))
+checkM_Km =tk.Radiobutton(window,text='m-km',command='sel')
+checkKm_M =tk.Radiobutton(window,text='km-m',command='sel')
 
 label = tk.Label( text="Welcome to the conversion calculator", pady=30, padx=100, font=100,)
 # entry=tk.Entry(window, textvariable= val, font = ('calibre',10,'normal'))
@@ -44,6 +47,10 @@ click_here = tk.Button(window, text="Click to Convert", command=submit, padx = 1
 
 
 
+label.pack()
+
+checkKm_M.pack()
+checkM_Km.pack()
 label.pack()
 num_entry.pack()
 click_here.pack()
