1ddbfadffe8352a3d6dfb2835ae04bb920739f1b
clamnam <n00211151@iadt.ie>
2023-09-30 14:13:15 +0100

    pace conversion

--- Calc.py
+++ Calc.py
@@ -1,6 +1,23 @@
+import datetime
 class Calculator:
-    def __init__(self, unit1, unit2):
-        self.unit1 = unit1
-        self.unit2 = unit2
+    def __init__(self, dist,*time):
+        self.dist = dist
+        self.time = time
+
+
     
-    def 
+    def MilesKilometers(dist):
+        return dist * 1.609344
+    
+    def KilometersMiles(dist):
+        return dist * .621371
+    
+    def pace(dist,time):
+        mins, seconds = map(int, time.split(':'))
+        seconds = seconds+(mins*60)
+        minutedecimal = seconds/60
+        singlekmpace = minutedecimal/dist
+        singlekmpace = singlekmpace
+        minutes = int(singlekmpace)
+        secondseconds = int((singlekmpace - minutes) * 60)
+        return minutes,":",secondseconds ,"per km",
---
+++ main.py
@@ -0,0 +1,7 @@
+from Calc import Calculator as calc
+def main():
+    print(calc.pace(2,'3:45'))    
+    
+    
+if __name__ == "__main__":
+    main()
