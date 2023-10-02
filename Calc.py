e9d2997260d8d12d46aec0944ca6ee34bd6cf224
clamnam <n00211151@iadt.ie>
2023-10-02 15:25:42 +0100

    pace, and conversion working

--- Calc.py
+++ Calc.py
@@ -1,23 +1,43 @@
-import datetime
 class Calculator:
-    def __init__(self, dist,*time):
+    def __init__(self, dist,time):
         self.dist = dist
         self.time = time
 
-
-    
-    def MilesKilometers(dist):
-        return dist * 1.609344
-    
-    def KilometersMiles(dist):
-        return dist * .621371
-    
-    def pace(dist,time):
+    def SplitTime(self,time):
         mins, seconds = map(int, time.split(':'))
+        return mins,seconds
+    
+    def MilesKilometers(self,int):
+        return int * 1.609344
+    
+    def KilometersMiles(self,int):
+        return int * .621371
+    
+    def pace(self, dist , time):
+        mins,seconds = self.SplitTime(time)
         seconds = seconds+(mins*60)
         minutedecimal = seconds/60
         singlekmpace = minutedecimal/dist
         singlekmpace = singlekmpace
         minutes = int(singlekmpace)
         secondseconds = int((singlekmpace - minutes) * 60)
-        return minutes,":",secondseconds ,"per km",
+        return minutes,secondseconds 
+
+    def KmM(self,dist,time):
+        string = ''
+        pace = self.pace(dist,time)
+        pace = list(pace)
+        PaceInSeconds = pace[0]*60+pace[1]
+        PaceInSeconds = (self.KilometersMiles(PaceInSeconds))/60
+        
+        print(round(PaceInSeconds,2))
+        
+        
+
+    def MkM(self,dist,time):
+        pace = self.pace(dist,time)
+        pace = list(pace)
+        PaceInSeconds = pace[0]*60+pace[1]
+        PaceInSeconds = (self.MilesKilometers(PaceInSeconds))/60
+        
+        print()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from Calc import Calculator as calc
 def main():
-    print(calc.pace(2,'3:45'))    
-    
-    
+  dist=5
+  time='35:23'
+  cal = calc(dist,time)
+  print(cal.pace(dist,time))
+
 if __name__ == "__main__":
     main()
+    
+    
